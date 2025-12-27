9913c7d0bf5e17b421f279a2523d7c4d452c7747
hmovaghari <hmovaghari@gmail.com>
2025-12-27 17:18:13 +0330

    Change default route to use ProxyBrowser action
    Updated the default route in Program.cs so that the Home controller's ProxyBrowser action is now the default entry point instead of Index. Users navigating to the root URL will be directed to ProxyBrowser.

--- ViewBrowser/ViewBrowser/Program.cs
+++ ViewBrowser/ViewBrowser/Program.cs
@@ -70,7 +70,7 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Home}/{action=ProxyBrowser}/{id?}");
 
 app.Run();
 
